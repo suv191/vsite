82ce014b9151e60a5c019008812a1d3a8a082c13
suv191 <suv191@yahoo.co.in>
2016-07-04 17:32:55 +0530

    day one

---
+++ basic.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Assignment-1</title>
+	<link rel="stylesheet" href="basic.css">
+</head>
+<body>
+	<h1>thank You! A representive will contact you soon</h1>
+
+	<?php 	
+		$firstName = $_POST["firstname"];
+		$lastName = $_POST["lastname"];
+		$Radio = $_POST["radio"];
+		$contactInfo = $_POST["contact"];
+		$City = $_POST["city"];
+		$Comments = $_POST["comments"];
+	
+
+	
+
+	print"<p>Information Submitted for: 
+	</p>"
+
+	<p>
+		You Phone No is:	</br>
+		and You are interested in living in 
+	</p>
+	
+	<p>
+		Our representative will reveiw your comments below:
+	</p>
+
+	?>
+</body>
+</html>
